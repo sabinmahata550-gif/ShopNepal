d42b73481548dea30764eb46c4ee7defea7224a3
Full_stackAPP <sabinmahata550@gmail.com>
2026-09-22 16:19:19 +0545

    add new message

--- src/config/db.js
+++ src/config/db.js
@@ -4,7 +4,6 @@ import config from "./config.js";
 const connectDB = async () => {
     try {
         await mongoose.connect(config.MONGO_URI);
-
         console.log("MongoDB connected successfully");
     } catch (error) {
         console.error("Error connecting to MongoDB:", error.message);
